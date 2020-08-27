e043ca0ddf672750a4fc686e8a78c847fe2e9854
EunQ <eklee@LAPTOP-UKDH5731.localdomain>
2020-08-27 17:28:18 +0900

    2020-08-27 makeing protocolHandling in server.c

--- protocol.h
+++ protocol.h
@@ -1,20 +1,23 @@
 #define PROTOCOL_REQ_READY  1001
 #define PROTOCOL_ACK_READY  1002
-#define PROTOCOL_REQ_Sync    1003
-#define PROTOCOL_ACK_Sync    1004
+#define PROTOCOL_REQ_SYNC    1003
+#define PROTOCOL_ACK_SYNC    1004
 #define PROTOCOL_ACK_END    1005
 
+/*
 typedef struct _ReqReady{
     int protocolMode;
 }ReqReady;
 
+typedef struct _AckEnd{
+}_AckEnd;
+*/
+
 typedef struct _AckReady{
-    int protocolMode;
     int userId;
 }AckReady;
 
 typedef struct _ReqSync{
-    int protocolMode;
     int userId; //보낸 아이디
     int checkList;
     int subTotal;
@@ -23,14 +26,9 @@ typedef struct _ReqSync{
 }ReqSync;
 
 typedef struct _AckSync{
-    int protocolMode;
     int userId; //싱크를 맞춰야할 아이디
     int checkList;
     int subTotal;
     int bonus;
     int total;
 }AckSync;
-
-typedef struct _AckEnd{
-    int protocolMode;
-}_AckEnd;
--- server.c
+++ server.c
@@ -19,17 +19,63 @@ void errorHandling(char *msg){
     exit(1);
 }
 
-void * encodingProgocol(int protocalMode ,void * data){
-    void * res = NULL;  
+char revBuf[4096];
+int serverSocket = 0;
+int clientSocket[2] = {0};
 
+//서로 같은 운영체제에서 보낼꺼니 공백이나 엔디간 결과는 신경 쓰지 말자.
+void decodingProtocol(int protocolMode, char * data, void * voidRevData){
+    switch (protocolMode)
+    {
+    case PROTOCOL_REQ_SYNC:
+        memcpy(voidRevData, data, sizeof(ReqSync));
+        break;
+    default:
+        printf("wrong protocol mode in decoding %d\n", protocolMode);
+        break;
+    }
+}
 
+void protocolHandling(int protocolMode, int gameCnt, int recvId, void * voidRevData){
+    //짝수는 0, 홀수는 1 idx가 가리키는 소켓으로 보낸다. recvId는 서버에 요청을 보낸 id
+
+    switch (protocolMode)
+    {
+    case PROTOCOL_REQ_READY:
+        AckReady ackReady;
+        ackReady.userId =  recvId;  
+        write(clientSocket[recvId], (void *)&ackReady, sizeof(ackReady));
+        break;
+    case PROTOCOL_REQ_SYNC:
+        
+        break;
+    default:
+        printf("wrong protocol mode in handling %d\n", protocolMode);
+        break;
+    }
+}
+
+/*
+char * encodingProgocol(int protocolMode ,void * data){
+    char * res = NULL;  
+    switch (protocolMode)
+    {
+    case PROTOCOL_REQ_READY:
+        ReqReady * sendData = (ReqReady *) data;
+        break;
+    case PROTOCOL_REQ_Sync:
+        ReqSync * sendData = (ReqSync *) data;
+        break;
+    default:
+        printf("wrong protocol mode %d\n", protocolMode);
+        break;
+    }
     return res;
 }
+*/
 
 int main(int argc, char **argv){
 
-    int serverSocket = 0;
-    int clientSocket[2] = {0};
     struct sockaddr_in serverAddr;
     struct sockaddr_in clientAddr[2];
     int clientAddrSize = 0;
@@ -61,12 +107,19 @@ int main(int argc, char **argv){
     
     clientAddrSize = sizeof(clientAddr[0]);
     for(i=0;i<2;i++){
+        int protocolMode= 0;
         clientSocket[i] = accept(serverSocket, (struct sockaddr*)&(clientAddr[i]), &clientAddr);
         if(clientSocket[i] == -1){
             fprintf(stderr,"%d ", i);
             errorHandling("accept() error");
         }
+        int cnt = read(clientSocket[i], &protocolMode, sizeof(int));
+        if(cnt != sizeof(int)){
+            errorHandling("first. read() != int error");
+            exit(1);
+        }
 
+        send(clientSocket[i], (void *))
         void * data = encodingProgocol(PROTOCOL_ACK_READY, (void *){1});
         //write(clientSocket[i], )
     }
